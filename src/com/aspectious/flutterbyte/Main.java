046549212ef2f1d59525ee6bed013e70b2520619
Aspectious <griffin.siebert14@outlook.com>
2024-01-11 14:50:53 -0500

    Initial commit

---
+++ src/com/aspectious/flutterbyte/Main.java
@@ -0,0 +1,55 @@
+package com.aspectious.flutterbyte;
+
+import java.awt.Color;
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
+import javax.swing.*;
+
+public class Main {
+	private static JFrame window;
+	public static void main(String[] args) {
+		window = new JFrame();
+		window.setSize(256, 256);
+		ImageIcon img = new ImageIcon(Main.class.getResource(
+                "/res/bf.gif"));
+		JLabel lbln = new JLabel(img);
+		window.add(lbln);
+		window.setLocation(500, 500);
+		window.setUndecorated(true);
+		window.setBackground(new Color(1.0f,1.0f,1.0f,0.0f));
+		window.setAlwaysOnTop(true);
+		window.setTitle("Flutterbyte");
+		window.pack();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setVisible(true);
+		
+		GraphicsDevice gd = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
+		int width = gd.getDisplayMode().getWidth();
+		int height = gd.getDisplayMode().getHeight();
+		
+		while (true) {
+			int dir = (int)(Math.random() * 4);
+			int amt = (int)(Math.random()*5+1);
+			if (dir ==0) window.setLocation(window.getX(), window.getY()-amt);
+			if (dir ==1) {
+				window.setLocation(window.getX()-amt, window.getY());
+
+			}
+			if (dir ==2) window.setLocation(window.getX(), window.getY()+amt);
+			if (dir ==3) {
+				window.setLocation(window.getX()+amt, window.getY());
+			}
+			
+			if (window.getX()<-128) {
+				window.setLocation(width, window.getY());
+			} else if (window.getX()>(width-128)) {
+				window.setLocation(-128, window.getY());
+			}
+			if (window.getY()<-128) {
+				window.setLocation(window.getX(),height);
+			} else if (window.getY()>(height-128)) {
+				window.setLocation(window.getX(),-128);
+			}
+		}
+	}
+}
---
+++ src/module-info.java
@@ -0,0 +1,3 @@
+module Flutterbyte {
+	requires java.desktop;
+}
